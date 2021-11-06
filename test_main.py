68eecaeecb727d7ab063ee27f805445e3a638070
stephane <stephaneleonel@gmail.com>
2021-11-06 16:51:18 +0100

    unit test execution

--- test_main.py
+++ test_main.py
@@ -21,5 +21,4 @@ class TestJSONLoaderMethods(unittest.TestCase):
         self.assertEqual(self.movies[0]['id'], 'tt0111161')
 
 if __name__ == '__main__':
-    runner = xmlrunner.XMLTestRunner(output='reports')
-    unittest.main(testRunner=runner)
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='reports'))
